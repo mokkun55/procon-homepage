fc1b79e0c469a3e9bfd81b2e870efa9bdbc38e04
mm <m17mm1007.1007@gmail.com>
2024-11-25 20:50:27 +0900

    update: main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Noto_Sans_JP } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const noto = Noto_Sans_JP({
+  weight: ["400", "700"],
+  style: "normal",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -25,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${noto.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+import Image from "next/image";
+import "./page.css";
+
 const Home = () => {
   return (
     <div>
-      <h1>もっくん</h1>
-      <h2>プロコンホームページ</h2>
-      {/* 箇条書きで紹介 */}
-      <ul>
-        <li>プログラミングコンテストの情報をまとめています</li>
-        <li>アルゴリズムの問題を解いてみましょう</li>
-        <li>プログラミングのスキルを向上させましょう</li>
-      </ul>
+      <div id="top">
+        <div id="top-container">
+          <a href="https://www.ktc.ac.jp/">
+            <Image
+              src="/kindai-logo.png"
+              width={30}
+              height={30}
+              alt="kutc-logo"
+              className="inline-block align-middle mr-2"
+            />
+            <h1 className="inline-block align-middle">
+              近畿大学工業高等専門学校
+            </h1>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
