6868eb963e12543b6a1b6c1230b51153321db50e
mm <m17mm1007.1007@gmail.com>
2024-12-16 17:55:38 +0900

    Setup: Mantine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import styles from './layout.module.scss'
 import { Noto_Sans_JP } from 'next/font/google'
 import { Header } from '@/components/Header'
 import { SideBar } from '@/components/SideBar'
+import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core'
 
 // フォント指定
 const noto = Noto_Sans_JP({
@@ -14,15 +15,20 @@ const noto = Noto_Sans_JP({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
       <body className={`${noto.variable} antialiased`}>
-        <div className={styles.container}>
-          <Header />
-          <div className={styles.contents}>
-            <SideBar className={styles.sidebar} />
-            <div className={styles.content}>{children}</div>
+        <MantineProvider>
+          <div className={styles.container}>
+            <Header />
+            <div className={styles.contents}>
+              <SideBar className={styles.sidebar} />
+              <div className={styles.content}>{children}</div>
+            </div>
           </div>
-        </div>
+        </MantineProvider>
       </body>
     </html>
   )
