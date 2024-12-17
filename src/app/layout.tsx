6d663f73e945394892c6cbed33d1d3e45668bcb5
Wizamari1138 <g03106@ktc.ac.jp>
2024-12-17 11:16:45 +0900

    feat. footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import styles from './layout.module.scss'
 import { Noto_Sans_JP } from 'next/font/google'
 import { Header } from '@/components/Header'
 import { SideBar } from '@/components/SideBar'
+import { Footer } from '@/components/Footer'
 
 // フォント指定
 const noto = Noto_Sans_JP({
@@ -22,6 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <SideBar className={styles.sidebar} />
             <div className={styles.content}>{children}</div>
           </div>
+          <Footer />
         </div>
       </body>
     </html>
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import styles from './index.module.scss'
+import { Item } from '@/components/features/Item'
+import { Splitter } from '@/components/features/Splitter'
+
+export const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.toptext}>
+        <Link href="https://www.ktc.ac.jp/">
+          <p>近畿大学工業高等専門学校</p>
+        </Link>
+        <h1>プログラミング技術部</h1>
+        <Item url="/" text="ホーム" size="big" />
+      </div>
+      <div className={styles.footerColumn}>
+        <div className={styles.column}>
+          <Splitter text="入学生向け">
+            <Item url="/" text="入部届け" size="default" />
+            <Item url="/" text="活動日及び活動場所" size="default" />
+            <Item url="/" text="作品一覧" size="default" />
+          </Splitter>
+        </div>
+        <div className={styles.column}>
+          <Splitter text="部員向け">
+            <Item url="/" text="予定" size="default" />
+            <Item url="/" text="お知らせ" size="default" />
+            <Item url="https://procon-absence-form-3d1be.firebaseapp.com/" text="欠席連絡フォーム" size="default" />
+          </Splitter>
+        </div>
+        <div className={styles.column}>
+          <Splitter text="開発">
+            <Item url="/" text="開発の余談" size="default" />
+            <Item url="https://github.com/mokkun55/procon-homepage" text="Github" size="default" />
+          </Splitter>
+        </div>
+      </div>
+    </div>
+  )
+}
