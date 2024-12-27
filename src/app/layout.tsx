903876bee4bbf8f53df46cb4d0ee28bad89c578d
mm <m17mm1007.1007@gmail.com>
2024-12-28 01:16:05 +0900

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,43 @@
-import './globals.css'
-import styles from './layout.module.scss'
-import { Noto_Sans_JP } from 'next/font/google'
-import { Header } from '@/components/Header'
-import { SideBar } from '@/components/SideBar'
-import { Footer } from '@/components/Footer'
-import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core'
+import "./globals.css";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { SideBar } from "@/components/SideBar";
+import {
+	ColorSchemeScript,
+	MantineProvider,
+	mantineHtmlProps,
+} from "@mantine/core";
+import { Noto_Sans_JP } from "next/font/google";
+import styles from "./layout.module.scss";
 
 // フォント指定
 const noto = Noto_Sans_JP({
-  weight: ['400', '700'],
-  style: 'normal',
-  subsets: ['latin'],
-  variable: '--font-noto-sans-jp',
-})
+	weight: ["400", "700"],
+	style: "normal",
+	subsets: ["latin"],
+	variable: "--font-noto-sans-jp",
+});
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" {...mantineHtmlProps}>
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body className={`${noto.variable} antialiased`}>
-        <MantineProvider>
-          <div className={styles.container}>
-            <Header />
-            <div className={styles.contents}>
-              <SideBar className={styles.sidebar} />
-              <div className={styles.content}>{children}</div>
-            </div>
-          </div>
-          <Footer />
-        </MantineProvider>
-      </body>
-    </html>
-  )
+export default function RootLayout({
+	children,
+}: { children: React.ReactNode }) {
+	return (
+		<html lang="en" {...mantineHtmlProps}>
+			<head>
+				<ColorSchemeScript />
+			</head>
+			<body className={`${noto.variable} antialiased`}>
+				<MantineProvider>
+					<div className={styles.container}>
+						<Header />
+						<div className={styles.contents}>
+							<SideBar className={styles.sidebar} />
+							<div className={styles.content}>{children}</div>
+						</div>
+					</div>
+					<Footer />
+				</MantineProvider>
+			</body>
+		</html>
+	);
 }
