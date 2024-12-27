82c5c286f3210ce4e229480cda4fdbcc05023092
Wizamari1138 <g03106@ktc.ac.jp>
2024-12-27 20:20:03 +0900

    Add ESLint configuration and update TypeScript, React, and Sass dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+const path = require('path')
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  experimental: {
+    turbo: {}, // Turbopack を明示的に有効化
+  },
+}
 
-export default nextConfig;
+export default nextConfig
