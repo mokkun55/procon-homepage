c86160755d6cc84a5348cc9bc13e52d244dc77d6
もっくん <mokkunpc@gmail.com>
2025-01-03 22:26:39 +0900

    fix: jang en -> ja

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const noto = Noto_Sans_JP({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" {...mantineHtmlProps}>
+    <html lang="ja" {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
       </head>
