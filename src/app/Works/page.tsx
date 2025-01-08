aeaa8199f4b3c20a695226fdf32cdaaccf87ace4
taiyo1452 <hmtaiyo989@gmail.com>
2025-01-08 22:46:40 +0900

    css追加

--- src/app/Works/page.tsx
+++ src/app/Works/page.tsx
@@ -1,5 +1,10 @@
+import { Tags } from '@/components/ui/tags'
 import styles from './page.module.scss'
 
 export default function Works() {
-  return <div className={styles.container} />
+  return (
+    <div className={styles.container}>
+      <Tags text="Next.js" />
+    </div>
+  )
 }
