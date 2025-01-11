556c2665ef44d1017c3b90af7f43e101e5a9acbb
もっくん <mokkunpc@gmail.com>
2025-01-11 10:37:04 +0900

    del: uppercase files

--- src/app/Absence/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function Absence() {
-  redirect('https://procon-absence-form-3d1be.firebaseapp.com/')
-}
--- src/app/Schedule/page.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './page.module.scss'
-
-export default function Schedule() {
-  return <div className={styles.container} />
-}
