43dbfe4dfaec70a7504cd7791ae6515e24aed7a0
もっくん <mokkunpc@gmail.com>
2025-01-12 20:22:02 +0900

    feat: work conponents

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,13 +1,46 @@
 import { Work } from '@/components/features/Work'
-import Link from 'next/link'
+import { SimpleGrid } from '@mantine/core'
 import styles from './page.module.scss'
 
 export default function Works() {
   return (
+    // TODO 後々スケルトンとか表示したいな
     <div className={styles.container}>
-      <Link href="/works/work1">work1</Link>
-      <Link href="/works/work2">work2</Link>
-      <Work title="ほげ" date={new Date()} img="/404/404not-found.png" description="あいうえお" />
+      <div className={styles.header}>
+        <h1 className={styles.title}>作品紹介</h1>
+        <p className={styles.description}>画像クリックで詳細ページに移動できます</p>
+      </div>
+
+      <SimpleGrid cols={3} className={styles.works}>
+        <Work
+          title={'テスト'}
+          date={new Date()}
+          img={'/testImgs/chicken.jpg'}
+          description={'皆さん今日のお昼ごはんは、何を食べますか？？ 今日は、唐揚げを作りました！'}
+          id="work1"
+        />
+        <Work
+          title={'work2'}
+          date={new Date()}
+          img={'/404/404not-found.png'}
+          description={'皆さん今日のお昼ごはんは、何を食べますか？？ 今日は、唐揚げを作りました！ほげほげほげほげほげ'}
+          id="work2"
+        />
+        <Work
+          title={'3'}
+          date={new Date()}
+          img={'/404/404not-found.png'}
+          description={'皆さん今日のお昼ごはんは、何を食べますか？？ 今日は、唐揚げを作りました！'}
+          id="work3"
+        />
+        <Work
+          title={'4'}
+          date={new Date()}
+          img={'/404/404not-found.png'}
+          description={'皆さん今日のお昼ごはんは、何を食べますか？？ 今日は、唐揚げを作りました！'}
+          id="work4"
+        />
+      </SimpleGrid>
     </div>
   )
 }
--- src/components/features/Work/index.tsx
+++ src/components/features/Work/index.tsx
@@ -1,19 +1,31 @@
-import { Card, Image } from '@mantine/core'
-// import styles from './index.module.scss'
+'use client'
+
+import { Card } from '@mantine/core'
+import dayjs from 'dayjs'
+import { useRouter } from 'next/navigation'
+import styles from './index.module.scss'
 
 type Props = {
+  id: string
   title: string
   date: Date
   img: string
   description: string
 }
 
-export const Work = ({ title, date, img, description }: Props) => {
+export const Work = ({ title, date, img, description, id }: Props) => {
+  const formattedDate = dayjs(date).format('YYYY/MM/DD')
+  const router = useRouter()
   return (
-    <div>
-      <Card shadow="sm">
-        <Card.Section>alice</Card.Section>
-      </Card>
-    </div>
+    <Card withBorder className={styles.container} onClick={() => router.push(`/works/${id}`)}>
+      <div className={styles.cardTop}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.date}>{formattedDate}</p>
+      </div>
+      <img src={img} alt={title} />
+      <div className={styles.cardBottom}>
+        <p>{description}</p>
+      </div>
+    </Card>
   )
 }
