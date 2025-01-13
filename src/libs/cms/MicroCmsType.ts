d2d2dcd0fa2c6f01fb4130b69a2798f787e8108c
mm <m17mm1007.1007@gmail.com>
2025-01-13 10:27:49 +0900

    fix:all comment

--- src/libs/cms/MicroCmsType.ts
+++ src/libs/cms/MicroCmsType.ts
@@ -5,3 +5,7 @@ export type Works = {
   img: { url: string }
   description: string
 }
+
+export type Posts = {
+  
+}
--- src/libs/cms/microCmsClient.ts
+++ src/libs/cms/microCmsClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk'
 
-const client = () => {
+export const getClient = () => {
   if (!process.env.MICROCMS_API_KEY) {
     throw new Error('MicroCMSのAPIキーが設定されていません')
   }
@@ -11,20 +11,4 @@ const client = () => {
     serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
     apiKey: process.env.MICROCMS_API_KEY,
   })
-}
-
-export type Works = {
-  id: string
-  title: string
-  date: Date
-  img: { url: string }
-  description: string
-}
-
-export const getContents = async (endpoint: 'works') => {
-  const data = await client().get({
-    endpoint: endpoint,
-  })
-
-  return data.contents
 }
