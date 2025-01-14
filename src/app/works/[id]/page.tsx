659ea526975ca34099194886c99b49ea55c81743
もっくん <mokkunpc@gmail.com>
2025-01-14 21:33:05 +0900

    feat: fix tags styles & upate works/[id] page

--- src/app/works/[id]/page.tsx
+++ src/app/works/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { Tags } from '@/components/features/Tags'
+import styles from './page.module.scss'
+
 type Props = {
   params: Promise<{
     id: string
@@ -7,5 +10,14 @@ type Props = {
 export default async function page(props: Props) {
   const params = await props.params
   const { id } = params
-  return <div>{id}</div>
+  return (
+    <div className={styles.container}>
+      <p>{id}</p>
+      <div className={styles.tags}>
+        <Tags text="React.js" />
+        <Tags text="Next.js" />
+        <Tags text="TypeScript" />
+      </div>
+    </div>
+  )
 }
