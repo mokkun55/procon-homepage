de9add6ac0ad94e4efe68a703e4cf3a3cd0b64d4
もっくん <mokkunpc@gmail.com>
2025-01-15 10:21:48 +0900

    fix: carousel style

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -6,7 +6,6 @@ import { Image } from '@mantine/core'
 type Props = {
   images: string[]
   height: number
-  slideSize?: number
   slideGap?: string
   loop?: boolean
   withIndicators?: boolean
@@ -15,20 +14,13 @@ type Props = {
 export const Carousel = ({
   images,
   height,
-  slideSize = 70,
   slideGap = 'md',
   loop = true,
   withIndicators = true,
 }: Props): React.ReactNode => {
   console.log(images)
   return (
-    <MantineCarousel
-      slideSize={`${slideSize}%`}
-      height={height}
-      slideGap={slideGap}
-      loop={loop}
-      withIndicators={withIndicators}
-    >
+    <MantineCarousel height={height} slideGap={slideGap} loop={loop} withIndicators={withIndicators}>
       {images.map((image, index) => (
         <MantineCarousel.Slide key={image}>
           <Image src={image} height={height} alt={`carousel-image-${index}`} />
