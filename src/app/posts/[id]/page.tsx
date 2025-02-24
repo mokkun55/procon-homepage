dfecf053459ad11361fbd45071788be9202eaad7
mm <m17mm1007.1007@gmail.com>
2025-02-24 17:43:15 +0900

    feat: post detail page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,3 +1,9 @@
+import { getContent } from '@/feature/cms/hooks/MicroCmsContents'
+import type { PostsType } from '@/libs/cms/types/MicroCmsType'
+import dayjs from 'dayjs'
+import ReactHtmlParser, {} from 'react-html-parser'
+import styles from './page.module.scss'
+
 type Props = {
   params: Promise<{
     id: string
@@ -7,5 +13,18 @@ type Props = {
 export default async function page(props: Props) {
   const params = await props.params
   const { id } = params
-  return <div>{id}</div>
+  const content = (await getContent({ endpoint: 'posts', id: id })) as PostsType
+  const formattedDate = dayjs(content.date).format('YYYY/MM/DD')
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>{content.title}</p>
+      <div className={styles.contentContainer}>
+        <p className={styles.date}>{formattedDate}</p>
+        <p className={styles.contentTitle}>{content.description}</p>
+        <img className={styles.image} src={content.mainImage.url} alt={content.title} />
+        <div className={styles.content}>{ReactHtmlParser(content.content)}</div>
+      </div>
+    </div>
+  )
 }
