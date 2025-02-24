f837231a96c85e7a2cfe7954bf4a4b8f3ab4b15d
mm <m17mm1007.1007@gmail.com>
2025-02-25 05:13:12 +0900

    [WIP] feat:post detail

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,7 +1,9 @@
 import { getContent } from '@/feature/cms/hooks/MicroCmsContents'
 import type { PostsType } from '@/libs/cms/types/MicroCmsType'
 import dayjs from 'dayjs'
-import ReactHtmlParser, {} from 'react-html-parser'
+import parser, { type DOMNode, type Element } from 'html-react-parser'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { monokai } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import styles from './page.module.scss'
 
 type Props = {
@@ -10,11 +12,50 @@ type Props = {
   }>
 }
 
+type CmsElement = DOMNode &
+  Element & {
+    data: string
+  }
+
 export default async function page(props: Props) {
   const params = await props.params
   const { id } = params
   const content = (await getContent({ endpoint: 'posts', id: id })) as PostsType
   const formattedDate = dayjs(content.date).format('YYYY/MM/DD')
+  const parsedContent = parser(content.content, {
+    replace: (domNode) => {
+      const dom = domNode as CmsElement
+      if (dom.tagName !== 'pre') return domNode
+      const codeTag = dom.children[0] as CmsElement
+      const code = (codeTag.children[0] as CmsElement).data
+      const language = codeTag.attribs.class.replace('language-', '')
+      const parentTag = dom.parent as CmsElement
+      const fileNameAttribute = parentTag.attributes.find((attr) => attr.name === 'data-filename')
+      if (!fileNameAttribute)
+        return (
+          <div className={styles.codeContainer}>
+            <SyntaxHighlighter language={language} style={monokai}>
+              {code}
+            </SyntaxHighlighter>
+          </div>
+        )
+      const fileName = fileNameAttribute.value
+      return (
+        <div className={styles.codeContainer}>
+          <div className={styles.fileNameContainer}>
+            <span className={styles.fileName}>{fileName}</span>
+          </div>
+          <div className={styles.code}>
+            <SyntaxHighlighter language={language} style={monokai} showLineNumbers>
+              {/* TODO この文字列の長さで来ると全然バグる */}
+              {/* {code} */}
+              aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            </SyntaxHighlighter>
+          </div>
+        </div>
+      )
+    },
+  })
 
   return (
     <div className={styles.container}>
@@ -23,7 +64,7 @@ export default async function page(props: Props) {
         <p className={styles.date}>{formattedDate}</p>
         <p className={styles.contentTitle}>{content.description}</p>
         <img className={styles.image} src={content.mainImage.url} alt={content.title} />
-        <div className={styles.content}>{ReactHtmlParser(content.content)}</div>
+        <div className={styles.content}>{parsedContent}</div>
       </div>
     </div>
   )
