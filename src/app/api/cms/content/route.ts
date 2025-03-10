3eb89d22148c40a387a21a3413099578588f1095
もっくん <mokkunpc@gmail.com>
2025-03-10 23:28:11 +0900

    test

--- src/app/api/cms/content/route.ts
+++ src/app/api/cms/content/route.ts
@@ -2,6 +2,7 @@ import { getContent } from '@/feature/cms/hooks/MicroCmsContents'
 import { EndpointList, type MicroCmsContentsType } from '@/feature/cms/hooks/MicroCmsContentsTypes'
 import { type NextRequest, NextResponse } from 'next/server'
 
+// GET /api/cms/content
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const params = request.nextUrl.searchParams
   const endpoint = params.get('endpoint') as MicroCmsContentsType['endpoint']
