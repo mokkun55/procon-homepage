f710d07eb3e7d0a0629cbd716d7ebfd8ecc97f81
mm <m17mm1007.1007@gmail.com>
2025-04-14 10:59:51 +0900

    fix: comment

--- src/components/metadata.tsx
+++ src/components/metadata.tsx
@@ -8,16 +8,14 @@ export const pageMetadata = {
     title: '近畿大学工業高等専門学校プログラミング技術部',
     description:
       '近大高専プログラミング技術部です！1年から5年のみんなで高専プロコンや名張祭に向けて日々活動しています！部員募集中です！プログラミング未経験も是非！！！！',
-    // TODO 本番環境用がないから出来てから書く予定
-    //url: '',
+    url: 'https://ktc-procon.vercel.app',
     siteName: '近大高専プロコン部',
     images: [
       {
-        //TODO testImgsなので後で差し替え
         url: 'https://ktc-procon.vercel.app/images/top-carousel-1.png',
-        width: 1960,
+        width: 1920,
         height: 1080,
-        alt: 'ブログ記事の画像',
+        alt: 'プログラミング技術部紹介画像',
       },
     ],
     locale: 'ja_JP',
